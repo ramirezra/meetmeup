e0cee8da4ca11f4492cd44f15af53050c02ea9e3
Robinson Ramirez <robinson@camrpc.com>
2020-02-27 17:28:23 -0500

    Got graphql message working. Trying to extract single field.

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,42 +1,55 @@
 module Main exposing (main)
 
-import Api.Object exposing (User)
+import Api.Object.User
 import Api.Query as Query
+import Api.Scalar exposing (Id(..))
 import Api.ScalarCodecs exposing (Id)
 import Browser
 import Graphql.Http
 import Graphql.Operation exposing (RootQuery)
 import Graphql.SelectionSet as SelectionSet exposing (SelectionSet)
 import Html exposing (..)
+import PrintAny
 import RemoteData exposing (RemoteData)
+
+
+
+-- MODEL
 
 
 type alias Model =
     RemoteData (Graphql.Http.Error Response) Response
 
 
-type alias Response =
-    Id
-
-
-makeRequest : Cmd Msg
-makeRequest =
-    Query.user (Query.UserRequiredArguments "1") Api.Object.User
-        |> Graphql.Http.queryRequest "localhost:8080/graphql"
-        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
-
-
-type Msg
-    = GotResponse Model
-
-
 init : Flags -> ( Model, Cmd Msg )
 init _ =
-    ( RemoteData.Loading, makeRequest )
+    ( RemoteData.Loading, makeRequest (Id "3") )
 
 
 type alias Flags =
     ()
+
+
+type alias Response =
+    { username : String
+    , id : Id
+    }
+
+
+query : Id -> SelectionSet Response RootQuery
+query id =
+    Query.user { id = id } <|
+        SelectionSet.map2 Response
+            Api.Object.User.username
+            Api.Object.User.id
+
+
+makeRequest : Id -> Cmd Msg
+makeRequest id =
+    id
+        |> query
+        |> Graphql.Http.queryRequest "http://100.119.96.75:8080/query"
+        |> Graphql.Http.send (RemoteData.fromResult >> GotResponse)
 
 
 main : Program Flags Model Msg
@@ -49,9 +62,12 @@ main =
         }
 
 
-view : Model -> Html Msg
-view model =
-    div [] []
+
+--UPDATE
+
+
+type Msg
+    = GotResponse Model
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -59,6 +75,24 @@ update msg model =
     case msg of
         GotResponse response ->
             ( response, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ Html.input [] []
+        , div
+            []
+            [ PrintAny.view model ]
+        ]
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
